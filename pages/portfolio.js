62a660643e7678681fbd93aa12d9d499a54ef1b2
Paul <pdpadian@gmail.com>
2020-09-08 12:44:36 -0600

    track

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -15,7 +15,7 @@ export default function About() {
       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177380102-1"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+        function gtag(){dataLayer.push(arguments)};
         gtag('js', new Date());
 
         gtag('config', 'UA-177380102-1');
